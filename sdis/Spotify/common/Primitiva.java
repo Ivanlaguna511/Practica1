5a60f4d40a22f8ae52c48e9320659ecc244a3cf3
DesertZito <118901855+DesertZito@users.noreply.github.com>
2024-03-25 13:57:03 +0100

    Create Primitiva.java

---
+++ sdis/Spotify/common/Primitiva.java
@@ -0,0 +1,15 @@
+package sdis.Spotify.common;
+
+    public enum Primitiva {
+        INFO,
+        XAUTH,
+        ADDD2L,
+        ADDED,
+        READL,
+        MEDIA,
+        EMPTY,
+        DELETEL,
+        DELETED,
+        NOTAUTH,
+        ERROR,
+    }
