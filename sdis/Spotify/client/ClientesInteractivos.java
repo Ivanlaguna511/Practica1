1c903f20bd91bb3168ede246911076f67b8d13e1
DesertZito <118901855+DesertZito@users.noreply.github.com>
2024-03-28 13:21:34 +0100

    Update ClientesInteractivos.java

--- sdis/Spotify/client/ClientesInteractivos.java
+++ sdis/Spotify/client/ClientesInteractivos.java
@@ -1,87 +1,91 @@
-package sdis.Spotify.server;
+package sdis.Spotify.client;
+
+
 import sdis.Spotify.common.*;
-import sdis.utils.*;
 
-import sdis.Spotify.client.unit.*;
-import java.io.IOException;
-
-public class Sirviente implements Runnable {
-    private final java.net.Socket socket;
-    private final MultiMap<String, String> mapa;
-    private final java.io.ObjectOutputStream oos;
-    private final java.io.ObjectInputStream ois;
-    private final int ns;
-    private static java.util.concurrent.atomic.AtomicInteger nInstance =
-            new java.util.concurrent.atomic.AtomicInteger();
-
-    public Sirviente(java.net.Socket s, MultiMap<String, String> c) throws java.io.IOException {
-        this.socket = s;
-        this.mapa = c;
-        this.ns = nInstance.getAndIncrement();
-        this.oos = new java.io.ObjectOutputStream(socket.getOutputStream());
-        this.ois = new java.io.ObjectInputStream(socket.getInputStream());
-    }  //se invoca en el Servidor, usualmente
-
-    public void run() {
+public class ClientesInteractivos {
+    final private int PUERTO = 2000;
+    static java.io.ObjectInputStream ois = null;
+    static java.io.ObjectOutputStream oos = null;
+    public static void main(String[] args) throws java.io.IOException {
+        String linea = null;
+        boolean fin = false;
+        java.net.Socket sock = new java.net.Socket("localhost", 2000);
         try {
-            System.out.println("----Server waiting client----");
-            while (true) {
-
-                String mensaje;  //String multipurpose
-                MensajeProtocolo me = (MensajeProtocolo) ois.readObject();
-                MensajeProtocolo ms;
-                // me y ms: mensajes entrante y saliente
-                System.out.println("Sirviente: " + ns + ": [ME: " + me); // depuracion me
-                switch (me.getPrimitiva()) {
-                    case INFO:
-                    case ADDED:
-                    case EMPTY:
-                    case DELETED:
-                    case XAUTH:
-                        break;
-                    case MEDIA:
-                    case ERROR:
-                    case NOTAUTH:
-                    case READL:
-                        mensaje = mapa.pop(me.getIdCola());
-                        if (null != mensaje) {
-                            ms = new MensajeProtocolo(Primitiva.MEDIA, mensaje);
-                            System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
-                            oos.writeObject(ms);
-                    } else {
-                            ms = new MensajeProtocolo(Primitiva.EMPTY);
-                            System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
-                            oos.writeObject(ms);
-                    }
-                    break;
-                    case DELETEL:
-                        if (null != (mensaje = mapa.pop(me.getIdCola()))) {
-                            ms = new MensajeProtocolo(Primitiva.DELETED, mensaje);
-                            System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
-                            oos.writeObject(ms);
-                        } else {
-                            ms = new MensajeProtocolo(Primitiva.EMPTY);
-                            System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
-                            oos.writeObject(ms);
-                        }
-                        break;
-                    case ADD2L:
-                        mapa.push(me.getIdCola(), me.getMensaje());
-                        synchronized (mapa) {
-                            mapa.notify();
-                        }  //despierta un sirviente esperando en un bloqueo de "mapa"
-                        ms = new MensajeProtocolo(Primitiva.ADDED);
-                        System.out.println("Sirviente: " + ns + ": [ME: " + ms); // depuracion me
-                        oos.writeObject(ms);
-                        break;
-
-
-                }  //fin del selector segun el mensaje entrante
-
+            java.io.BufferedReader tec =
+                    new java.io.BufferedReader(
+                            new java.io.InputStreamReader(System.in));
+            //Creación de los canales de serialización de objetos
+            oos = new java.io.ObjectOutputStream(sock.getOutputStream());
+            ois = new java.io.ObjectInputStream(sock.getInputStream());
+            MensajeProtocolo minfo = (MensajeProtocolo) ois.readObject();
+            if (minfo.getPrimitiva().equals(Primitiva.ERROR)){
+                System.out.println(minfo);
+                fin = true;
+            }else{
+                System.out.println(minfo);
+            }
+            while(!fin){
+                System.out.println("Que desea hacer: 1-Loggear 2-Añadir canción a Playlist 3-Escuchar Playlist 4-Borrar Playlist ");
+                linea = tec.readLine();
+                if(linea.equals("1")){
+                  System.out.println("Usuario: ");
+                  String Usuario = tec.readLine();
+                  System.out.println("Contraseña: ");
+                  String Password = tec.readLine();
+                  pruebaPeticionRespuesta(new MensajeProtocolo(Primitiva.XAUTH, Usuario, Password));
+                  MensajeProtocolo mr = (MensajeProtocolo) ois.readObject();
+                  System.out.println(mr);
+                }
+                else if (linea.equals("2")){
+                    System.out.println("Playlist: ");
+                    String PlayList = tec.readLine();
+                    System.out.println("Canción: ");
+                    String Song = tec.readLine();
+                    pruebaPeticionRespuesta(new MensajeProtocolo(Primitiva.ADD2L, PlayList, Song));
+                    MensajeProtocolo mr = (MensajeProtocolo) ois.readObject();
+                    System.out.println(mr);
+                }
+                else if(linea.equals("3")){
+                    System.out.println("Playlist: ");
+                    String PlayList = tec.readLine();
+                    pruebaPeticionRespuesta(new MensajeProtocolo(Primitiva.READL, PlayList));
+                    MensajeProtocolo mr = (MensajeProtocolo) ois.readObject();
+                    System.out.println(mr);
+                }
+                else if(linea.equals("4")){
+                    System.out.println("Playlist: ");
+                    String PlayList = tec.readLine();
+                    pruebaPeticionRespuesta(new MensajeProtocolo(Primitiva.DELETEL, PlayList));
+                    MensajeProtocolo mr = (MensajeProtocolo) ois.readObject();
+                    System.out.println(mr);
+                }else{
+                    System.out.println("Elija una opción valida ");
+                }
 
             }
-        }catch(Exception io){
-
+            //a estas alturas algún cliente externo debería insertar un mensaje en la cola
+            //FIN Esceniario 1
+        } catch (java.io.EOFException e) {
+            System.err.println("Cliente: Fin de conexión.");
+        } catch (java.io.IOException e) {
+            System.err.println("Cliente: Error de apertura o E/S sobre objetos: "+e);
+        } catch (MalMensajeProtocoloException e) {
+            System.err.println("Cliente: Error mensaje Protocolo: "+e);
+        } catch (Exception e) {
+            System.err.println("Cliente: Excepción. Cerrando Sockets: "+e);
+        } finally {
+            ois.close();
+            oos.close();
+            sock.close();
         }
     }
+    //Prueba una interacción de escritura y lectura con el servidor
+    static void pruebaPeticionRespuesta(MensajeProtocolo mp) throws java.io.IOException, MalMensajeProtocoloException, ClassNotFoundException {
+        System.out.println("> "+mp);
+        oos.writeObject(mp);
+
+    }
 }
+
+
